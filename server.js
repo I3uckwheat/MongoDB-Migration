d657b35aea719beba704d5a0de72328f5fc4efd0
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-12 20:07:48 -0600

    connects to local mongo db

--- server.js
+++ server.js
@@ -2,5 +2,10 @@ const mongodb = require("mongodb")
 const async = require("async")
 const addresses = require("./CustomerData/m3-customer-data.json")
 const customers = require("./CustomerData/m3-customer-data.json");
+const url = "mongodb://localhost:27017/customer-data"
 
-console.log(addresses);
+mongodb.MongoClient.connect(url, (error, client) => {
+  if(error) throw new Error(error);
+  console.log("Connected to DB")
+  const db = client.db("customer-data")
+})
