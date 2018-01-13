27a80ec0a244a73b063fd65d863fee3d9aa477c1
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-12 23:04:01 -0600

    does proper operations

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mongodb = require("mongodb")
 const async = require("async")
-const addresses = require("./CustomerData/m3-customer-data.json")
+const addresses = require("./CustomerData/m3-customer-address-data.json")
 const customers = require("./CustomerData/m3-customer-data.json");
 const url = "mongodb://localhost:27017/customer-data"
 
@@ -8,4 +8,15 @@ mongodb.MongoClient.connect(url, (error, client) => {
   if(error) throw new Error(error);
   console.log("Connected to DB")
   const db = client.db("customer-data")
+
+  const startTime = Date.now();
+  customers.forEach((customer, index) => {
+    db.collection("customers").insertOne({
+      ...customer,
+      ...addresses[index]
+    }, console.log("finsihed"))
+  });
+  client.close();
+  const endTime = Date.now() - startTime;
+  console.log(endTime + "ms");
 })
