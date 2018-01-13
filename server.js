52a48bf2896ec0a1953ac056b2c3406d0850814a
Briggs Elsperger <i3uckwheat@gmail.com>
2018-01-12 20:03:13 -0600

    adds addresses and customers requirements

--- server.js
+++ server.js
@@ -1,2 +1,6 @@
 const mongodb = require("mongodb")
 const async = require("async")
+const addresses = require("./CustomerData/m3-customer-data.json")
+const customers = require("./CustomerData/m3-customer-data.json");
+
+console.log(addresses);
